8f7c7874eb07473543f7a02e9b29c307f30e6db8
Sarthak Mittal <sam.mittal.2106@gmail.com>
2021-09-28 20:07:55 +0530

    rebase and error handle

--- src/paths.h
+++ src/paths.h
@@ -4,6 +4,6 @@
 #include <string>
 
 #ifndef FILE_PATHS_H
-    #define INPUT_FILE_PATH "./data/rr_test3.csv"    
-    #define OUTPUT_FILE_PATH "./data/sample_output.csv"
+#define INPUT_FILE_PATH "./data/sample_input.csv"
+#define OUTPUT_FILE_PATH "./data/sample_output.csv"
 #endif
