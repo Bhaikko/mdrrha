0588866fd60d100bdb9bf214b8f5637d4c6f0b8d
Siddharth <siddhartha.pawar17@gmail.com>
2021-11-15 22:47:45 +0530

    Test cases and experiments results added

--- src/paths.h
+++ src/paths.h
@@ -6,11 +6,11 @@
 #ifndef FILE_PATHS_H
 // #define INPUT_FILE_PATH "./data/sample_input.csv"
 
-// Kaggle 1 File Paths
+// Kaggle 1 Experiment 500
 // #define INPUT_FILE_PATH "./data/kaggle1/kaggle1_filtered.csv"
 // #define OUTPUT_FILE_PATH "./data/kaggle1/kaggle1_output.csv"
 
-// Kaggle 2 File Paths
+// Kaggle 2 Experiment 1500
 // #define INPUT_FILE_PATH "./data/kaggle2/kaggle2_filtered.csv"
 // #define OUTPUT_FILE_PATH "./data/kaggle2/kaggle2_output.csv"
 
@@ -19,13 +19,28 @@
 // #define OUTPUT_FILE_PATH "./data/random/random_AT_NZ_output.csv"
 
 // Random File Path with AT Zero
-// #define INPUT_FILE_PATH "./data/random/random_AT_Z.csv"
-// #define OUTPUT_FILE_PATH "./data/random_AT_Z_output.csv"
+#define INPUT_FILE_PATH "./data/random/random_AT_Z.csv"
+#define OUTPUT_FILE_PATH "./data/random/random_AT_Z_output.csv"
 
-// DRRHA datasets
-#define INPUT_FILE_PATH "./data/drrha_2/drrha_case3.csv"
-#define OUTPUT_FILE_PATH "./data/drrha_2/drrha_2_output.csv"
+// DRRHA test case 1
+// #define INPUT_FILE_PATH "./data/drrha_1/drrha1_case2.csv"
+// #define OUTPUT_FILE_PATH "./data/drrha_1/drrha_1_output.csv"
 
+// DRRHA test case 2
+// #define INPUT_FILE_PATH "./data/drrha_2/drrha2_case3.csv"
+// #define OUTPUT_FILE_PATH "./data/drrha_2/drrha_2_output.csv"
+
+// // DRRHA test case 3
+// #define INPUT_FILE_PATH "./data/drrha_3/drrha3_case3.csv"
+// #define OUTPUT_FILE_PATH "./data/drrha_3/drrha_3_output.csv"
+
+// // DRRHA test case 4
+// #define INPUT_FILE_PATH "./data/drrha_4/drrha4_case4.csv"
+// #define OUTPUT_FILE_PATH "./data/drrha_4/drrha_4_output.csv"
+
+// HRRNA NDRRHA Comparison
+// #define INPUT_FILE_PATH "./data/ndrrha_hrrnha/ndrrha_hrrnha6.csv"
+// #define OUTPUT_FILE_PATH "./data/ndrrha_hrrnha/ndrrha_hrrnha_output.csv"
 
 
 #define RANDOM_DATASET_FILE_PATH "./data/random_with_AT_zero.csv"
